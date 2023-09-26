7398ac6103a8ec9bb9435d004046c3e8acf571e3
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-09-26 16:40:25 +0400

    Create RecentBlogs.tsx

---
+++ components/RecentBlogs/RecentBlogs.tsx
@@ -0,0 +1,15 @@
+// RecentBlogs.tsx
+import React from 'react';
+
+const RecentBlogs: React.FC = () => {
+    return (
+        <section className="bg-black text-white py-16">
+            <div className="container mx-auto text-center">
+                <h2 className="text-2xl font-bold mb-4">Recent Blogs</h2>
+                {/* Add a list of recent blog posts here */}
+            </div>
+        </section>
+    );
+};
+
+export default RecentBlogs;
