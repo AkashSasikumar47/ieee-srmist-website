27bb7c4a314cf7327dcbea33fa352e53ebe58185
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-09-26 16:40:23 +0400

    Create PopularEvents.tsx

---
+++ components/PopularEvents/PopularEvents.tsx
@@ -0,0 +1,15 @@
+// PopularEvents.tsx
+import React from 'react';
+
+const PopularEvents: React.FC = () => {
+    return (
+        <section className="bg-black text-white py-16">
+            <div className="container mx-auto text-center">
+                <h2 className="text-2xl font-bold mb-4">Popular Events</h2>
+                {/* Add a list of popular events here */}
+            </div>
+        </section>
+    );
+};
+
+export default PopularEvents;
