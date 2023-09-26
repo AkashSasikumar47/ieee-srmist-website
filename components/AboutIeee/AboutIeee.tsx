5b71cd5ac46433d86a3521b33f47b5011c74cee3
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-09-26 16:40:15 +0400

    Create AboutIeee.tsx

---
+++ components/AboutIeee/AboutIeee.tsx
@@ -0,0 +1,17 @@
+// AboutIeee.tsx
+import React from 'react';
+
+const AboutIeee: React.FC = () => {
+    return (
+        <section className="bg-black text-white py-16">
+            <div className="container mx-auto text-center">
+                <h2 className="text-2xl font-bold mb-4">About IEEE SRMIST Student Branch</h2>
+                <p className="text-lg">
+                    Your mission and vision statements and a brief introduction can go here.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default AboutIeee;
