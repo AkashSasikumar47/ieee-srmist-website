745714afc6dcb3d670b85ac117ca6f2b3316cdb1
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-09-26 16:15:42 +0400

    Create Navbar.tsx

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,44 @@
+// components/Navbar.tsx
+import React from 'react';
+import Link from 'next/link';
+
+const Navbar: React.FC = () => {
+    return (
+        <nav className="bg-blue-500 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* Logo */}
+                <div className="text-white font-bold text-xl">
+                    <Link href="/">
+                        <a>IEEE SRMIST</a>
+                    </Link>
+                </div>
+
+                {/* Navigation Links */}
+                <ul className="flex space-x-4">
+                    <li>
+                        <Link href="/about">
+                            <a className="text-white hover:underline">About Us</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/events">
+                            <a className="text-white hover:underline">Events</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/blog">
+                            <a className="text-white hover:underline">Blog</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">
+                            <a className="text-white hover:underline">Contact</a>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
