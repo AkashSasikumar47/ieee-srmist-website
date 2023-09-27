46382886c7bd7004fa79105b189cfceb5eefc15e
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-09-27 14:18:07 +0400

    Create about.tsx

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+// pages/about.tsx
+import React from 'react';
+import Navbar from '../components/Home/Navbar/Navbar';
+
+const AboutPage: React.FC = () => {
+    return (
+        <div>
+            <Navbar />
+
+        </div>
+    );
+};
+
+export default AboutPage;
