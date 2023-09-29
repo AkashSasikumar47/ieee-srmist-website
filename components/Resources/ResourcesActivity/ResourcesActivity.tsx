3a31ad6f2e12348526fd3cac16e05f652bf6559f
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-09-29 20:20:52 +0400

    Update Code

--- components/Resources/ResourcesActivity/ResourcesActivity.tsx
+++ components/Resources/ResourcesActivity/ResourcesActivity.tsx
@@ -2,32 +2,63 @@ import React from 'react';
 
 const ResourcesActivity: React.FC = () => {
     return (
-        <section className="bg-black text-white py-16" style={{ width: '1440px' }}>
+        <div>
+            {/* Desktop layout */}
+            <section className="bg-black text-white py-16 hidden sm:block">
+                <div className="w-[1440px] h-[1162px] px-[84px] py-16 flex-col justify-start items-center gap-2.5 inline-flex">
+                    <div className="self-stretch h-[512px] rounded-lg justify-start items-center inline-flex">
+                        <img className="w-[636px] h-[512px] relative" src="https://via.placeholder.com/636x512" alt="Career Guidance" />
+                        <div className="grow shrink basis-0 self-stretch px-[54px] py-16 flex-col justify-center items-start gap-8 inline-flex">
+                            <div className="self-stretch text-white text-[32px] font-bold font-['Montserrat']">Career Guidance</div>
+                            <div className="self-stretch text-white text-base font-normal font-['Montserrat'] tracking-tight">Navigate your career path with IEEE's career development resources. Whether you're a student exploring potential career options or a professional looking to enhance your skills, you'll find valuable guidance, webinars, and articles to help you achieve your career goals.</div>
+                            <div className="px-4 py-1.5 bg-white rounded justify-center items-center gap-2.5 inline-flex">
+                                <div className="text-center text-black text-sm font-semibold font-['Montserrat']">View More</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="self-stretch h-[512px] rounded-lg justify-start items-center inline-flex">
+                        <div className="grow shrink basis-0 self-stretch px-[54px] py-16 flex-col justify-center items-start gap-8 inline-flex">
+                            <div className="self-stretch text-white text-[32px] font-bold font-['Montserrat']">Industry Standards</div>
+                            <div className="self-stretch text-white text-base font-normal font-['Montserrat'] tracking-tight">Access IEEE's collection of industry standards and guidelines, ensuring that your projects and research align with the best practices in your field. These standards cover a wide range of technical areas, from telecommunications to electrical safety.</div>
+                            <div className="px-4 py-1.5 bg-white rounded justify-center items-center gap-2.5 inline-flex">
+                                <div className="text-center text-black text-sm font-semibold font-['Montserrat']">View More</div>
+                            </div>
+                        </div>
+                        <img className="w-[636px] h-[512px] relative" src="https://via.placeholder.com/636x512" alt="Industry Standards" />
+                    </div>
+                </div>
+            </section>
 
-            <div className="w-[1440px] h-[1162px] px-[84px] py-16 flex-col justify-start items-center gap-2.5 inline-flex">
-                <div className="self-stretch h-[512px] rounded-lg justify-start items-center inline-flex">
-                    <img className="w-[636px] h-[512px] relative" src="https://via.placeholder.com/636x512" />
-                    <div className="grow shrink basis-0 self-stretch px-[54px] py-16 flex-col justify-center items-start gap-8 inline-flex">
-                        <div className="self-stretch text-white text-[32px] font-bold font-['Montserrat']">Career Guidance</div>
-                        <div className="self-stretch text-white text-base font-normal font-['Montserrat'] tracking-tight">Navigate your career path with IEEE's career development resources. Whether you're a student exploring potential career options or a professional looking to enhance your skills, you'll find valuable guidance, webinars, and articles to help you achieve your career goals.</div>
-                        <div className="px-4 py-1.5 bg-white rounded justify-center items-center gap-2.5 inline-flex">
-                            <div className="text-center text-black text-sm font-semibold font-['Montserrat']">View More</div>
+            {/* Mobile layout */}
+            <section className="bg-black text-white py-8 block sm:hidden">
+                <div className="w-full px-4">
+                    <div className="text-center text-blue-500 text-l font-bold font-montserrat">Resources Activity</div>
+                </div>
+
+                <div className="max-w-screen-md mx-auto px-4 py-4 flex flex-col items-center">
+                    <div className="w-full max-w-[400px] rounded-lg overflow-hidden flex-col justify-start items-start inline-flex mx-auto bg-white mb-4">
+                        <img className="w-full h-[200px] relative" src="https://via.placeholder.com/636x512" alt="Career Guidance" />
+                        <div className="w-full p-4 flex-col justify-start items-start gap-2 inline-flex mt-2">
+                            <div className="text-zinc-300 text-xs font-semibold font-['Montserrat']">Career Guidance</div>
+                            <div className="text-black text-base font-normal font-['Montserrat'] tracking-tight">Navigate your career path with IEEE's career development resources. Whether you're a student exploring potential career options or a professional looking to enhance your skills, you'll find valuable guidance, webinars, and articles to help you achieve your career goals.</div>
+                            <div className="bg-white rounded justify-center items-center gap-2.5 inline-flex">
+                                <div className="text-center text-black text-sm font-semibold font-['Montserrat']">View More</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full max-w-[400px] rounded-lg overflow-hidden flex-col justify-start items-start inline-flex mx-auto bg-white mb-4">
+                        <img className="w-full h-[200px] relative" src="https://via.placeholder.com/636x512" alt="Industry Standards" />
+                        <div className="w-full p-4 flex-col justify-start items-start gap-2 inline-flex mt-2">
+                            <div className="text-zinc-300 text-xs font-semibold font-['Montserrat']">Industry Standards</div>
+                            <div className="text-black text-base font-normal font-['Montserrat'] tracking-tight">Access IEEE's collection of industry standards and guidelines, ensuring that your projects and research align with the best practices in your field. These standards cover a wide range of technical areas, from telecommunications to electrical safety.</div>
+                            <div className="bg-white rounded justify-center items-center gap-2.5 inline-flex">
+                                <div className="text-center text-black text-sm font-semibold font-['Montserrat']">View More</div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="self-stretch h-[512px] rounded-lg justify-start items-center inline-flex">
-                    <div className="grow shrink basis-0 self-stretch px-[54px] py-16 flex-col justify-center items-start gap-8 inline-flex">
-                        <div className="self-stretch text-white text-[32px] font-bold font-['Montserrat']">Industry Standards</div>
-                        <div className="self-stretch text-white text-base font-normal font-['Montserrat'] tracking-tight">Access IEEE's collection of industry standards and guidelines, ensuring that your projects and research align with the best practices in your field. These standards cover a wide range of technical areas, from telecommunications to electrical safety.</div>
-                        <div className="px-4 py-1.5 bg-white rounded justify-center items-center gap-2.5 inline-flex">
-                            <div className="text-center text-black text-sm font-semibold font-['Montserrat']">View More</div>
-                        </div>
-                    </div>
-                    <img className="w-[636px] h-[512px] relative" src="https://via.placeholder.com/636x512" />
-                </div>
-            </div>
-
-        </section>
+            </section>
+        </div>
     );
 };
 
--- pages/resources.tsx
+++ pages/resources.tsx
@@ -15,13 +15,29 @@ const ResourcesPage: React.FC = () => {
         <div>
             <Navbar />
 
-            <section className="bg-black text-white py-16" style={{ width: '1440px' }}>
-                <div className="w-[1440px] h-[621px] px-[84px] pt-64 pb-16 bg-black flex-col justify-start items-center gap-16 inline-flex">
+            <section className="bg-black text-white py-16">
+
+                {/* Desktop layout */}
+                <div className="hidden sm:block max-w-screen-xl mx-auto px-4 sm:px-0 pt-64 pb-16 bg-black flex-col justify-start items-center gap-16">
                     <div className="self-stretch text-white text-8xl font-bold font-['Montserrat']">RESOURCES</div>
-                    <div className="w-[1440px] pl-[800px] pr-[84px] justify-start items-start gap-2.5 inline-flex">
-                        <div className="grow shrink basis-0 text-white text-base font-normal font-['Montserrat'] tracking-tight">Explore a wealth of resources curated to support your academic and professional growth. From educational materials and cutting-edge research to career development tools and industry standards, our IEEE resources are here to empower your journey to success. Dive in and discover the knowledge that will shape your future.</div>
+                    <div className="w-full max-w-screen-xl mx-auto px-4 pt-3 pb-16 bg-black flex-col justify-start items-center gap-16 inline-flex">
+                        <div className="w-full max-w-screen-xl mx-auto px-4 pt-3 pb-16 bg-black flex-col justify-start items-center gap-8 inline-flex">
+                            <div className="self-stretch h-[174px] pr-[84px] lg:pl-[800px] lg:pr-[84px] pt-3 pb-16 bg-black flex-col justify-start items-center gap-8 inline-flex">
+                                <div className="grow shrink basis-0 text-white text-base font-normal font-['Montserrat'] tracking-tight">Explore a wealth of resources curated to support your academic and professional growth. From educational materials and cutting-edge research to career development tools and industry standards, our IEEE resources are here to empower your journey to success. Dive in and discover the knowledge that will shape your future.</div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
+
+                {/* Mobile layout */}
+                <div className="block sm:hidden mx-auto px-4 py-8 bg-black text-center">
+                    <div className="text-5xl font-bold font-montserrat">RESOURCES</div>
+                    <div className="mt-8">
+                        <p className="text-xl font-normal font-['Montserrat'] tracking-tight">Explore a wealth of resources curated to support your academic and professional growth. From educational materials and cutting-edge research to career development tools and industry standards, our IEEE resources are here to empower your journey to success. Dive in and discover the knowledge that will shape your future.</p>
+                    </div>
+                </div>
+
             </section>
 
             <ResourcesXplore />
