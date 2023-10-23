a673cdf3419d3f09fc481a11de562560922aba50
Sreeram3927 <sreeram292004@gmail.com>
2023-10-23 13:16:13 +0530

    render the images

--- components/Gallery/GalleryImages.tsx
+++ components/Gallery/GalleryImages.tsx
@@ -2,24 +2,18 @@ import React from 'react';
 import GalleryImage from "./Image";
 
 const GalleryImages: React.FC = () => {
+
+    const images = [];
+    for (let index = 1; index <= 12; index++) {
+        images.push(
+            <GalleryImage key={index} imageUrl={`/assets/images/${index}.jpg`} />
+        );
+    }
+
     return (
         <div className="max-w-[1440px] mx-auto text-white">
             <div className="max-w-screen-lg mx-auto p-4 flex flex-wrap justify-center">
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
-                <GalleryImage />
+                {images}
             </div>
         </div>
     );
--- components/Gallery/Image.tsx
+++ components/Gallery/Image.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
 
-const GalleryImage: React.FC = () => {
+interface GalleryImageProps {
+    imageUrl: string;
+}
+
+const GalleryImage: React.FC<GalleryImageProps> = ({imageUrl}) => {
     return (
         <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-5">
             <div className="relative" style={{ paddingBottom: '125%' }}>
-                <img className="absolute inset-0 w-full h-full object-cover border-8 border-white" src="https://via.placeholder.com/400x500" />
+                <img
+                    className="absolute inset-0 w-full h-full object-cover border-8 border-white"
+                    src={imageUrl} 
+                    alt='no img'
+                    style={{
+                        borderRadius: '8px',
+                    }}
+                />
             </div>
         </div>
     )
